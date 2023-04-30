0bf1dc94b573b983cbf1d7ce7935039d8a5e361a
jimmy <jmetz2510@gmail.com>
2023-04-30 00:16:53 -0400

    Add buttons to create/reset all boxes

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 const boxes = document.querySelectorAll(".box");
 
+function createBoxes(numOfBoxes) {
+
+    let newRow = document.createElement("div");
+    let container = document.getElementById("container");
+    newRow.classList.add("row");
+    container.appendChild(newRow);
+
+    for (i = 0; i < numOfBoxes; i++) {
+        let newBox = document.createElement("div");
+        newBox.classList.add("box");
+        newRow.appendChild(newBox);
+        newBox.addEventListener('mouseover', (event) => {
+            event.target.classList.add("highlighted");
+        });
+    }
+
+}
+
+
+function changeLayout() {
+    let newLayout = prompt("Enter the new layout:");
+    console.log(newLayout);
+    if (newLayout > 100) {
+        alert("Choose a number lower than 100.");
+        return
+    } else {
+        let allRows = document.querySelectorAll(".row");
+        allRows.forEach((row) => {
+            row.classList.add("disappear")
+        })
+
+        let i = 0;
+        while (i < newLayout) {
+            createBoxes(newLayout);
+            i++;
+        }
+    }
+};
+
+function resetBoxes() {
+    let allBoxes = document.querySelectorAll(".box");
+    allBoxes.forEach((box) => {
+        box.classList.remove("highlighted");
+    });
+}
+
+
+
 boxes.forEach(item => {
     item.addEventListener('mouseover', (event) => {
-        event.target.classList.add("highlighted")
-    })
-})
+        event.target.classList.add("highlighted");
+    });
+});
